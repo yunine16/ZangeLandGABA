d86525aeab14b11c971e0d6dfb59ddeb13da14d7
フジ <rockhopper2516@gmail.com>
2021-12-11 15:11:59 +0900

    フォント導入、PP導入、SEシステム構築、懺悔取得改良

---
+++ Assets/Fuji/UnityToNCMB/GetRandomZange.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCMB;
+using TMPro;
+
+public class GetRandomZange : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI infoText = null;
+    //zangesåüçıÇÃÇΩÇﬂÇ…ÅANCMBQueryçÏê¨
+    NCMBQuery<NCMBObject> zangesQuery = new NCMBQuery<NCMBObject>("Zange");
+    bool initialized = false;
+    private string resultString = "init";
+
+    private void Awake()
+    {
+        zangesQuery.FindAsync((List<NCMBObject> allZanges, NCMBException e) =>
+        {
+            if (e != null)
+            {
+                resultString = "error";
+            }
+            else
+            {
+                int zangeObjectNumber = Random.Range(0, allZanges.Count);
+                resultString = allZanges[zangeObjectNumber]["zangeText"].ToString();
+            }
+        });
+    }
+    public string GetZange()
+    {
+        zangesQuery.FindAsync((List<NCMBObject> allZanges, NCMBException e) =>
+        {
+            if (e != null)
+            {
+                resultString = "error";
+            }
+            else
+            {
+                int zangeObjectNumber = Random.Range(0, allZanges.Count);
+                resultString = allZanges[zangeObjectNumber]["zangeText"].ToString();
+            }
+        });
+        return resultString;
+    }
+}
